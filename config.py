707594d18836a6dcdb3dc464348d8f0661926874
Arthur Victory <arthurvictory@outlook.com>
2024-07-27 13:16:55 -0500

    Finished React Car Inventory

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
-import os 
+import os
 from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-
-class COnfig():
+class Config():
     '''
         Set config variables for the flask app
-        using ENvironment variables where available
-        Otherwise, create the config variable if not done already.
+        Using Environment variables where available.
+        Otherwise create the config variable if not done already
     '''
 
 
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    SECRET_KET = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ryan will never get access to my CSS'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_NOTIFICATIONS = False
+    SQLALCHEMY_TRACK_NOTIFICAITONS = False
